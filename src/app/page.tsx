c948e45b322810a6b426c16d9b3c5a093993cc06
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-09-12 17:19:13 -0300

    height on phone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="overflow-hidden w-full h-screen relative flex">
+    <main className="overflow-hidden w-full relative flex  max-h-[calc(100dvh)]">
       <div className="hidden flex-shrink-0 bg-gray-900 md:flex md:w-[260px] md:flex-col"></div>
       <Chat />
   </main>
